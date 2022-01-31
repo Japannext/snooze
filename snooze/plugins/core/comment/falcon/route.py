c7002e82ed004dc569f2eb167920f8606aad6a64
Florian Dematraz <florian.dematraz@japannext.co.jp>
2022-01-31 15:58:41 +0900

    Escalate regardless of the modification

--- snooze/plugins/core/comment/falcon/route.py
+++ snooze/plugins/core/comment/falcon/route.py
@@ -62,12 +62,9 @@ class CommentRoute(Route):
                         modification_raw = req_media.get('modifications', [])
                         if media_type in ['esc', 'open']:
                             try:
-                                modified = False
                                 for modification in modification_raw:
-                                    mod_obj = get_modification(modification, self.core)
-                                    if mod_obj.modify(records['data'][0]):
-                                        modified = True
-                                if modified and self.notification_plugin:
+                                    get_modification(modification, self.core).modify(records['data'][0])
+                                if self.notification_plugin:
                                     records['data'][0]['notification_from'] = notification_from
                                     self.notification_plugin.process(records['data'][0])
                             except Exception as e:
